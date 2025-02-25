7b9a3175c3ec176967d9e1e7c9b9b7064c0d066a
Adam McKee <adam.be.g84d@gmail.com>
2025-02-25 17:14:45 -0600

    fix gh release

--- .github/workflows/changelog_get.sh
+++ .github/workflows/changelog_get.sh
@@ -4,4 +4,4 @@ set -e
 FILE=$1
 VERSION=$2
 
-awk -v p="## $VERSION" -F":" '$0 ~ p{f=1;next} /## /{f=0} f' "$FILE" | grep .
+awk -v p="## $VERSION" -F":" '$0 ~ p{f=1;next} /## /{f=0} f' "$FILE" | grep . || true
